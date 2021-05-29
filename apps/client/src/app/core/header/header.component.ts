ec314ed14459ff69aac574e67e8b63e859e4d664
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-05-29 16:28:19 -0700

    Add header component

---
+++ apps/client/src/app/core/header/header.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'bt-header',
+  templateUrl: './header.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
