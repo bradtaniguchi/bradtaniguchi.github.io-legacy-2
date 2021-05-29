3b1108c60c02dc10d3b0f0394209525b0f16ca5c
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-05-29 16:40:58 -0700

    Add scully library to client

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([], {
+      initialNavigation: 'enabled',
+      paramsInheritanceStrategy: 'always',
+    }),
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ scully.client.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/client/src",
+  projectName: "client",
+  outDir: './dist/static',
+  routes: {
+  }
+};
