a1f3a3407c1c2a3f208d1d00d38559171d10b399
brad taniguchi <bradtaniguchi@gmail.com>
2021-05-30 12:07:20 -0700

    Attempt to fix octicon directive
    - Issues found with this approach, might need to take a component route.

--- apps/client/src/app/core/octicon/octicon.directive.ts
+++ apps/client/src/app/core/octicon/octicon.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as octicons from '@primer/octicons';
 
@@ -9,44 +15,27 @@ import * as octicons from '@primer/octicons';
 export class OcticonDirective implements OnInit {
   @Input() octicon?: string;
 
-  constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef) {
+  constructor(
+    private sanitizer: DomSanitizer,
+    private elementRef: ElementRef,
+    private cdr: ChangeDetectorRef
+  ) {
     console.log('WTF');
   }
 
   ngOnInit() {
     if (!this.octicon) return;
     const octicon = octicons[this.octicon];
+    console.log('octicon', octicon);
     if (!octicon) return;
     if (this.octicon) {
-      this.elementRef.nativeElement.innerHtml = this.sanitizer.bypassSecurityTrustHtml(
+      (this.elementRef
+        .nativeElement as HTMLElement).innerHTML = this.sanitizer.bypassSecurityTrustHtml(
         octicon.toSVG()
-      );
+      ) as string;
+
+      console.log('test with native el', this.elementRef.nativeElement);
+      this.cdr.detectChanges();
     }
   }
 }
-
-// @Input()
-// svg?: string;
-
-// constructor(
-//   private _elementRef: ElementRef,
-// ) {}
-
-// ngOnChanges(changes: SimpleChanges) {
-//   if (changes.svg) {
-//     this._elementRef.nativeElement.innerHTML = '';
-
-//     if (this.svg) {
-//       this._elementRef.nativeElement.innerHTML = this.svg;
-//     }
-//   }
-// }
-
-// newSteps=[];
-// constructor(private sanitizer: DomSanitizer) { }
-
-// ngOnInit() {
-//     for (let i = 0; i < this.steps.length; i++) {
-//       this.newSteps.push(this.sanitizer.bypassSecurityTrustHtml(this.steps[i].icon));
-//     }
-// }
