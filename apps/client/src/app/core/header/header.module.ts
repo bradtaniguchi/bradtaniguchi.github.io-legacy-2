987c150e78f58f0edc6a230699e6237ae034ffe0
Brad <bradtaniguchi@gmail.com>
2021-05-30 19:47:46 +0000

    Add link setup for header component

--- apps/client/src/app/core/header/header.module.ts
+++ apps/client/src/app/core/header/header.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { OcticonModule } from '../octicon/octicon.module';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [OcticonModule],
+  imports: [
+    // Core Imports
+    RouterModule,
+    // Display Imports
+    OcticonModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
