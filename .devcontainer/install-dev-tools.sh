befe14394dd7013927f30a3e11c0e8fc7e187f87
Brad <bradtaniguchi@gmail.com>
2021-05-30 19:52:00 +0000

    Update codespaces workspace settings

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set vim as git editor
+git config --global core.editor "vim"
+
+# set vim as other editors in general
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+# setup git completion
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
+
+# Setup local dev environment
+npm ci
+
+# Get latest from repo
+git fetch
