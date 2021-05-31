c735b017c43a2c9f1f2ea38bf3ae536a70e5022f
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-05-31 16:33:28 -0700

    Update main layout of home page
    - need to add more information for the cards.

--- apps/client/src/app/modules/home/home.module.ts
+++ apps/client/src/app/modules/home/home.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { OcticonModule } from '../../core/octicon/octicon.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
+    // core modules
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    // Display Imports
+    OcticonModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
