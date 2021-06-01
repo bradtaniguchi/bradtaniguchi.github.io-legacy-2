a00edf02c31d5a8ad33af632eb0df264693a8bde
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-05-31 19:53:18 -0700

    Add json-generator commanderjs based tool

--- tools/json-generator/src/main.ts
+++ tools/json-generator/src/main.ts
@@ -1,2 +1,36 @@
-console.log('Hello World!');
+import { Command } from 'commander';
+
+const program = new Command();
+
+// TODO: might need to have this script "wait"
+// example call:
+// nx run json-generator:generate --user
+program
+  .allowExcessArguments(true)
+  // generate build information
+  .command('git:info')
+  .option('--file <filepath>', 'path of the file to output to')
+  .action((options: { file?: string }) => {
+    console.log('[git info] called', { options });
+  })
+
+  // get codewars user information
+  .command('codewars:user')
+  .option('--user <username>', 'the codewars username to get')
+  .option('--file <filepath>', 'path of the file to output to')
+  .action((options: { user?: string; file?: string }) => {
+    console.log('[codwars user] called', { options });
+  })
+
+  // get the codewars challenge information
+  .command('codewars:challenges')
+  .option('--user <username>', 'the codewars username to get')
+  .option('--file <filepath>', 'path of the file to output to')
+  .action((options: { user?: string; file?: string }) => {
+    console.log('[codewars challenges] called', { options });
+  });
+
+// TODO: add github-api information
+// TODO: get github-gist information
+
 process.exit(0);
