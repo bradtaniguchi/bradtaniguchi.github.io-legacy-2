677c49219399abebeed9316b2421223554929621
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-06-02 20:40:12 -0700

    Add partial logic for get-git-info

--- tools/json-generator/src/main.ts
+++ tools/json-generator/src/main.ts
@@ -1,36 +1,41 @@
+#!npx ts-node
 import { Command } from 'commander';
+import { getGitInfo } from './app/get-git-info/get-git-info';
 
-const program = new Command();
+(async () => {
+  const program = new Command();
+  // TODO: might need to have this script "wait"
+  // example call to run every command:
+  // nx run json-generator:generate --user=bradtaniguchi
+  // or specific calls:
+  // ./tools/json-generator/src/main.ts git:info --file=<filename>
+  program
+    .allowExcessArguments(true)
+    // generate build information
+    .command('git:info')
+    .requiredOption('--file <filepath>', 'path of the file to output to')
+    .action(getGitInfo);
 
-// TODO: might need to have this script "wait"
-// example call:
-// nx run json-generator:generate --user
-program
-  .allowExcessArguments(true)
-  // generate build information
-  .command('git:info')
-  .option('--file <filepath>', 'path of the file to output to')
-  .action((options: { file?: string }) => {
-    console.log('[git info] called', { options });
-  })
+  program
+    // get codewars user information
+    .command('codewars:user')
+    .requiredOption('--user <username>', 'the codewars username to get')
+    .requiredOption('--file <filepath>', 'path of the file to output to')
+    .action((options: { user?: string; file?: string }) => {
+      console.log('[codwars user] called', { options });
+    });
 
-  // get codewars user information
-  .command('codewars:user')
-  .option('--user <username>', 'the codewars username to get')
-  .option('--file <filepath>', 'path of the file to output to')
-  .action((options: { user?: string; file?: string }) => {
-    console.log('[codwars user] called', { options });
-  })
+  program
+    // get the codewars challenge information
+    .command('codewars:challenges')
+    .requiredOption('--user <username>', 'the codewars username to get')
+    .requiredOption('--file <filepath>', 'path of the file to output to')
+    .action((options: { user?: string; file?: string }) => {
+      console.log('[codewars challenges] called', { options });
+    });
+  // TODO: add github-api information
+  // TODO: get github-gist information
 
-  // get the codewars challenge information
-  .command('codewars:challenges')
-  .option('--user <username>', 'the codewars username to get')
-  .option('--file <filepath>', 'path of the file to output to')
-  .action((options: { user?: string; file?: string }) => {
-    console.log('[codewars challenges] called', { options });
-  });
-
-// TODO: add github-api information
-// TODO: get github-gist information
-
-process.exit(0);
+  await program.parseAsync(process.argv);
+  process.exit(0);
+})();
