95f2c81938f0fddc4aef463ac77a75cd13fbb4c1
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-06-12 15:10:11 -0700

    Add theme service, with localstorage support

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SidenavStoreService } from './core/sidenav/sidenav.store.service';
+import { ThemeStoreService } from './store/theme/theme.store.service';
 
 @Component({
   selector: 'bt-root',
   templateUrl: './app.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [SidenavStoreService],
+  providers: [SidenavStoreService, ThemeStoreService],
 })
 export class AppComponent {}
---
+++ apps/client/src/app/store/theme/theme.store.service.ts
@@ -0,0 +1,64 @@
+import { Inject, Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { LOCAL_FORAGE } from '../../core/local-forage/local-forage';
+import { Theme, ThemeState } from './theme.state';
+
+@Injectable()
+export class ThemeStoreService extends ComponentStore<ThemeState> {
+  private static THEME_STORAGE_KEY = 'bt-app-theme';
+  public static THEMES: Theme[] = ['light', 'dark'];
+  /**
+   * The theme that is currently selected within the state.
+   */
+  public theme$ = this.select((state) => state.theme);
+
+  constructor(@Inject(LOCAL_FORAGE) private localforage: LocalForage) {
+    super();
+
+    // get and set the default theme if available
+    this.localforage
+      .getItem(ThemeStoreService.THEME_STORAGE_KEY)
+      .then((theme: Theme | unknown) =>
+        this.isTheme(theme) ? this.setTheme(theme) : this.setTheme('dark')
+      )
+      .catch((err) => console.error(err));
+  }
+  /**
+   * Directly set the theme of the app.
+   */
+  private readonly setTheme = this.updater((state, theme: Theme) => ({
+    ...state,
+    theme,
+  }));
+  /**
+   * Set the theme into localStorage
+   */
+  private readonly setThemeInLocalStorage$ = this.effect(
+    (theme$: Observable<Theme>) =>
+      theme$.pipe(
+        tap((theme) =>
+          this.localforage.setItem(ThemeStoreService.THEME_STORAGE_KEY, theme)
+        )
+      )
+  );
+
+  private isTheme(theme: unknown): theme is Theme {
+    return ThemeStoreService.THEMES.includes(theme as Theme);
+  }
+
+  /**
+   * Changes the theme from dark to light mode.
+   *
+   * **note** this will be removed if we end up supporting more themes
+   */
+  public toggleTheme() {
+    const themeToSet$ = this.theme$.pipe(
+      // **note** this may change, if there are more themes in the future
+      map((currentTheme) => (currentTheme === 'dark' ? 'light' : 'dark'))
+    );
+    this.setTheme(themeToSet$);
+    this.setThemeInLocalStorage$(themeToSet$);
+  }
+}
