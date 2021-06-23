7464ac380e3944dc6101d417b951c825d7e4495b
Brad <bradtaniguchi@gmail.com>
2021-06-22 17:01:47 -0700

    Fix issue with with all links from link-box (#44)

--- apps/client/src/app/modules/home/home.module.ts
+++ apps/client/src/app/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LinkBoxComponent } from './link-box/link-box.component';
 import { HomeProfileComponent } from './home-profile/home-profile.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent, LinkBoxComponent, HomeProfileComponent],
   imports: [
     // core modules
     CommonModule,
+    RouterModule,
     HomeRoutingModule,
     // Display Imports
     OcticonModule,
