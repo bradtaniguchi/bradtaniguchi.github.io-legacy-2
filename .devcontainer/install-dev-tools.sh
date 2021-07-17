14a8d320b4539de5cdbcc953516caf6b96ea913a
Brad <bradtaniguchi@gmail.com>
2021-07-17 18:05:00 +0000

    Update container build
    - include cypress dependencies
    - include lighthouse chrome install

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -13,26 +13,29 @@ echo "export EDITOR=\"$VISUAL\"" >> ~/.bashrc
 # setup git completion
 echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 
-# Setup local dev environment
-npm ci
-
 # Get latest from repo
 git fetch
 
 # install and setup nvm version
-nvm use
 nvm install
+nvm use
 npm config delete prefix
 nvm alias default node
 
-# install global nx cli, not always required but useful to have
-npm i -g @nrwl/cli
+# install cypress dependencies
+apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 
 # we need google-chrome to run lighthouse tests, install it
-# mkdir ./tmp
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# mv google-chrome-stable_current_amd64.deb ./google-chrome-stable_current_amd64.deb
-# sudo apt install ./tmp/google-chrome-stable_current_amd64.deb
+mkdir ./tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+mv google-chrome-stable_current_amd64.deb ./tmp/google-chrome-stable_current_amd64.deb
+sudo apt install ./tmp/google-chrome-stable_current_amd64.deb
 
 # clean up  codespace install
-# rm -rf ./tmp
+rm -rf ./tmp
+
+# install npm packages
+npm ci
+
+# install global nx cli, not always required but useful to have
+npm i -g @nrwl/cli
