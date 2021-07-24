b1c738dc00762c4f40df7d19b54e7e6f58d754b6
Brad <bradtaniguchi@gmail.com>
2021-07-24 19:11:33 +0000

    Add list-common to all list pages
    - will be updated with new features shortly after getting refactored and
    storybooked

--- apps/client/src/app/modules/projects-list/projects-list.module.ts
+++ apps/client/src/app/modules/projects-list/projects-list.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContentWrapperModule } from '../../core/content-wrapper/content-wrapper.module';
+import { ListCommonModule } from '../../shared/list-common/list-common.module';
 import { ProjectsListRoutingModule } from './projects-list-routing.module';
 import { ProjectsListComponent } from './projects-list.component';
 
 @NgModule({
   declarations: [ProjectsListComponent],
-  imports: [CommonModule, ProjectsListRoutingModule, ContentWrapperModule],
+  imports: [
+    CommonModule,
+    ProjectsListRoutingModule,
+    ContentWrapperModule,
+    ListCommonModule,
+  ],
 })
 export class ProjectsListModule {}
--- apps/client/src/app/modules/snippets-list/snippets-list.module.ts
+++ apps/client/src/app/modules/snippets-list/snippets-list.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContentWrapperModule } from '../../core/content-wrapper/content-wrapper.module';
+import { ListCommonModule } from '../../shared/list-common/list-common.module';
 import { SnippetsListRoutingModule } from './snippets-list-routing.module';
 import { SnippetsListComponent } from './snippets-list.component';
 
 @NgModule({
   declarations: [SnippetsListComponent],
-  imports: [CommonModule, SnippetsListRoutingModule, ContentWrapperModule],
+  imports: [
+    CommonModule,
+    SnippetsListRoutingModule,
+    ContentWrapperModule,
+    ListCommonModule,
+  ],
 })
 export class SnippetsListModule {}
