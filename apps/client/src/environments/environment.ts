8ef6934e4720b363f20f5a5bdb6899c84e89ef1a
Brad <bradtaniguchi@gmail.com>
2021-09-19 19:53:24 +0000

    Add Google analytics
    Closes #173

--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   githubRepo: 'bradtaniguchi.github.io',
   codewarsUser: 'bradtaniguchi',
   freeCodeCampUser: 'bradtaniguchi',
+  gtagCode: 'G-C61SZ9GLMC',
   // these are social media urls:
   linkedIn: 'https://www.linkedin.com/in/bradley-taniguchi-62a052104/',
   npm: 'https://www.npmjs.com/~bradtaniguchi',
