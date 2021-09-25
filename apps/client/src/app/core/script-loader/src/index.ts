0345eb354b6c96aa31c39d8157f2749f3eae61c3
Brad Taniguchi <bradtaniguchi@gmail.com>
2021-09-25 14:52:32 -0700

    add script-loader code
    - moved from another project as-is. Needs work
    
    [skip  ci]

---
+++ apps/client/src/app/core/script-loader/src/index.ts
@@ -0,0 +1,5 @@
+export * from './script-loader-init-scripts';
+export * from './script-loader-loaded';
+export * from './script-loader.module';
+export * from './script-loader.service';
+export * from './script-params';
