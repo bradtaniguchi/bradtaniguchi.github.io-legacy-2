90d9dad53725e4f154b8d28289b436b3cb012e8c
Brad <bradtaniguchi@gmail.com>
2021-11-03 00:05:40 +0000

    Update purgecss script

--- tools/purgecss.sh
+++ tools/purgecss.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-echo 'starting purgecss.sh'
+echo "starting purgecss.sh..."
+
+echo "changing directory to ./dist/static"
 cd ./dist/static
 
-pwd
+echo "Getting CSS_FILE variable"
+CSS_FILE=`find * -iname styles.*.css`
+echo "found file:" $CSS_FILE
 
-CSS_FILE=`find * -iname *.css`
-echo purging $CSS_FILE
-
+echo "creating temp_css folder"
 mkdir -p temp_css
 
-npx purgecss --css='$CSS_FILE' --output=./temp_css --content="./**/*.html"
+echo "running purgecss for file " $CSS_FILE
+npx purgecss --css=$CSS_FILE --output=./temp_css --content ./**/*.html ./**/*.js
 
+# TODO: re-add later
 mv ./temp_css/$CSS_FILE ./
+
+echo "removing temp_css folder"
+rmdir ./temp_css
