b4c802520c8e774299d4fe4b8556eed39c041956
Brad <bradtaniguchi@gmail.com>
2022-03-05 14:17:41 -0800

    Update page-title to just be the url (#211)
    - easier to track

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+import { ClientLoggerService } from './core/client-logger/client-logger.service';
 import { Theme } from './store/theme/theme.state';
 import { ThemeStoreService } from './store/theme/theme.store.service';
+
+// eslint-disable-next-line no-var
+declare var gtag: unknown;
 
 @Component({
   selector: 'bt-root',
@@ -26,15 +33,14 @@ export class AppComponent {
   /**
    * The data attribute used for both the dark and dark_dimmed themes
    */
-  public dataDarkTheme$: Observable<
-    'dark' | 'dark_dimmed' | undefined
-  > = this.theme$.pipe(
-    map((theme) =>
-      theme && (['dark', 'dark_dimmed'] as Array<Theme>).includes(theme)
-        ? (theme as 'dark' | 'dark_dimmed')
-        : undefined
-    )
-  );
+  public dataDarkTheme$: Observable<'dark' | 'dark_dimmed' | undefined> =
+    this.theme$.pipe(
+      map((theme) =>
+        theme && (['dark', 'dark_dimmed'] as Array<Theme>).includes(theme)
+          ? (theme as 'dark' | 'dark_dimmed')
+          : undefined
+      )
+    );
   /**
    * The data attribute used for the light theme or set to undefined
    */
@@ -42,5 +48,29 @@ export class AppComponent {
     map((theme) => (theme === 'light' ? 'light' : undefined))
   );
 
-  constructor(private themeStore: ThemeStoreService) {}
+  constructor(
+    private themeStore: ThemeStoreService,
+    private router: Router,
+    private logger: ClientLoggerService,
+    private title: Title
+  ) {
+    if (environment.gtagCode) {
+      this.logger.debug('gtag code provided, tracking page views');
+      this.router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .subscribe((event) => {
+          if (!gtag || typeof gtag !== 'function') {
+            this.logger.error('missing gtag', { gtag });
+            return;
+          }
+
+          this.logger.debug('page path', { event, router: this.router });
+          gtag('config', environment.gtagCode, {
+            page_path: (event as NavigationEnd).urlAfterRedirects,
+            page_location: (event as NavigationEnd).urlAfterRedirects,
+            page_title: (event as NavigationEnd).urlAfterRedirects,
+          });
+        });
+    }
+  }
 }
